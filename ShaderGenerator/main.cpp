2ad37c937afca1533530caf710c6899ee808231a
Peter Major <peter.major@cae.com>
2021-02-08 14:23:39 +0100

    WIP

---
+++ ShaderGenerator/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "exprtk.hpp"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+  init_apartment();
+  Uri uri(L"http://aka.ms/cppwinrt");
+  printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+
+  exprtk::parser<int> parser;
+  exprtk::expression<int> expression;
+  if (!parser.compile("true or false", expression))
+  {
+    printf("fail");
+  }
+  else
+  {
+    auto result = expression.value();
+    printf("success");
+  }
+}
