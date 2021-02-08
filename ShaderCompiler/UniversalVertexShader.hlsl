bc650aeaf6cf9863799eceb07ae7bcc6b0427804
axodox <axodox@outlook.com>
2021-02-08 22:29:05 +0100

    Removed obsolete code

--- ShaderCompiler/UniversalVertexShader.hlsl
+++
@@ -1,7 +0,0 @@
-//opt: colored | textured, fallback, multi_texture (!colored), flat | lambert (colored)
-//opt: colored | textured, ground_clip, transparency, multi_texture (!colored), overlay
-
-float4 main( float4 pos : POSITION ) : SV_POSITION
-{
-	return pos;
-}
