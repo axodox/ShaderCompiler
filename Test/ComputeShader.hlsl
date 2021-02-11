59ee0497c44a7c50b122f6d52dd3adba77e02d61
axodox <axodox@outlook.com>
2021-02-11 04:04:05 +0100

    Fixes

--- Test/ComputeShader.hlsl
+++ Test/ComputeShader.hlsl
@@ -1,13 +1,11 @@
 #pragma target cs_5_0
-#pragma entry main
-#pragma namespace MyApp::Shaders
-#pragma option bool IS_X
-#pragma option enum EN {val1, val2, val3}
-#pragma option int Y {1..12}
-#pragma asd
+#pragma option enum EdgeClip {None, RemoveOverflow, BlackOverflow}
+#pragma option bool GroundClip
+#pragma option bool EnvironmentMapping
+#pragma option enum Transparency {Full, Threshold}
 
 [numthreads(1, 1, 1)]
 void main(uint3 DTid : SV_DispatchThreadID)
 {
-  asdfadfgasasd asd asd asd
+
 }
